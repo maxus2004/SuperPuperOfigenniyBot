66aaf6a6bf684cdfadc4b1569a9dd620ce0751ff
Дорошин Максим <maxus2004123@gmail.com>
2021-06-17 18:55:36 +0300

    added support for multiple chats

--- SuperPuperOfigenniyBot/Program.cs
+++ SuperPuperOfigenniyBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
@@ -13,22 +14,19 @@ namespace SuperPuperOfigenniyBot
 {
     class Program
     {
-        private AudioStream discordAudioStream;
-        private bool busy = false;
         private DiscordSocketClient discord;
-        private IAudioClient audioClient;
-        private bool speaking = false;
-
+        private Dictionary<ulong, AudioStream> audioStreams = new Dictionary<ulong, AudioStream>();
+        private Dictionary<ulong, IAudioClient> audioClients = new Dictionary<ulong, IAudioClient>();
+        private Dictionary<ulong, bool> canSpeak = new Dictionary<ulong, bool>();
         static void Main()
         {
-            Console.WriteLine("starting bot...");
             new Program().MainAsync().GetAwaiter().GetResult();
         }
         async Task MainAsync()
         {
             discord = new DiscordSocketClient();
             discord.Log += DiscordDotNetLog;
-            await discord.LoginAsync(TokenType.Bot, "ODUzNjEwNjk3NTY0NDg3Njgy.YMX"+"46Q.85kBjRY4Or0ic4k4vRjdv_mnWIw");
+            await discord.LoginAsync(TokenType.Bot, "ODUzNjEwNjk3NTY0NDg3Njgy.YMX" + "46Q.85kBjRY4Or0ic4k4vRjdv_mnWIw");
             await discord.StartAsync();
             discord.MessageReceived += MessageReceivedHandler;
             await Task.Delay(-1);
@@ -36,50 +34,42 @@ namespace SuperPuperOfigenniyBot
 
         private async Task MessageReceivedHandler(SocketMessage arg)
         {
+            ulong channelId = arg.Channel.Id;
+            bool speaking = audioClients.ContainsKey(channelId);
+
             if (arg.Content == "/глаголь")
             {
                 if (speaking) return;
+
+                var voiceChannel = (arg.Author as IGuildUser).VoiceChannel;
+                if (voiceChannel == null)
+                {
+                    await arg.Channel.SendMessageAsync("Зайдите в голосовой чат чтобы написать /глаголь");
+                    return;
+                }
+
                 new Thread(() =>
                 {
-                    var voiceChannel = (arg.Author as IGuildUser).VoiceChannel;
-                    if (voiceChannel == null) return;
-                    audioClient = voiceChannel.ConnectAsync().Result;
-                    discordAudioStream = audioClient.CreatePCMStream(AudioApplication.Voice);
-                    speaking = true;
+                    audioClients.Add(channelId, voiceChannel.ConnectAsync().Result);
+                    audioStreams.Add(channelId, audioClients[channelId].CreatePCMStream(AudioApplication.Voice));
+                    canSpeak.Add(channelId,true);
                 }).Start();
             }
             else if (arg.Content == "/завались")
             {
                 if (!speaking) return;
 
-                while (busy)
-                {
-                    Thread.Sleep(50);
-                }
-                busy = true;
-                Stream voice = TextToSpeech("ну ладно, заваливаюсь");
-                await voice.CopyToAsync(discordAudioStream);
-                voice.Close();
-                busy = false;
+                await Speak(channelId, "Ну ладно, заваливаюсь");
 
                 await Task.Delay(1000);
-
-                speaking = false;
-
-                await audioClient.StopAsync();
+                await audioClients[channelId].StopAsync();
+                audioClients.Remove(channelId);
+                audioStreams.Remove(channelId);
+                canSpeak.Remove(channelId);
             }
             else if (speaking)
             {
-
-                while (busy)
-                {
-                    Thread.Sleep(50);
-                }
-                busy = true;
-                Stream voice = TextToSpeech(arg.Content);
-                await voice.CopyToAsync(discordAudioStream);
-                voice.Close();
-                busy = false;
+                _ = Speak(channelId, arg.Content).ConfigureAwait(false);
             }
         }
         private Task DiscordDotNetLog(LogMessage msg)
@@ -92,15 +82,27 @@ namespace SuperPuperOfigenniyBot
         {
             string pathOrUrl = "https://translate.google.com/translate_tts?ie=UTF-8&tl=Ru-ru&client=tw-ob&q=" + WebUtility.UrlEncode(text);
             var process = Process.Start(new ProcessStartInfo
-            { 
+            {
                 FileName = "ffmpeg",
-                Arguments = $"-i {pathOrUrl} " + 
-                            "-f s16le -ar 48000 -ac 2 pipe:1",
+                Arguments = $"-hide_banner -loglevel error -i {pathOrUrl} -f s16le -ar 48000 -ac 2 pipe:1",
                 UseShellExecute = false,
                 RedirectStandardOutput = true
             });
 
             return process.StandardOutput.BaseStream;
+        }
+
+        async Task Speak(ulong channelId, string text)
+        {
+            Stream voice = TextToSpeech(text);
+            while (!canSpeak[channelId])
+            {
+                await Task.Delay(50);
+            }
+            canSpeak[channelId] = false;
+            await voice.CopyToAsync(audioStreams[channelId]);
+            voice.Close();
+            canSpeak[channelId] = true;
         }
     }
 }
